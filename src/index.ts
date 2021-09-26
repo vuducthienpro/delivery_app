cd50c722cf8c4e10f39b4e14ee1d4953b79c3718
chungpv <chungpv@saisystem.vn>
2021-09-26 23:22:00 +0700

    setup login google

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,15 @@ import express from 'express';
 import dotenv from 'dotenv';
 import logger from 'morgan';
 import path from 'path';
+import cookieSession from 'cookie-session';
+import passport from 'passport';
+import passportGoogle from 'passport-google-oauth2';
+import mongoose from 'mongoose';
+import config from './config/options';
+const GoogleStrategy = passportGoogle.Strategy;
 
 // routers
-import indexRouter from './routes/index';
+// import indexRouter from './routes/index';
 import runningRouter from './routes/running';
 import crawlRouter from './routes/crawl';
 
@@ -15,12 +21,100 @@ dotenv.config();
 
 const app = express();
 
+mongoose
+  .connect(process.env.DB_URL, config.mongo.options)
+  .then((result) => {
+    winston.info('connect success1');
+  })
+  .catch((error) => {
+    winston.info('error');
+  });
+
+app.use(
+  cookieSession({
+    name: 'google-auth-session',
+    keys: ['key1', 'key2'],
+    maxAge: 5000,
+  }),
+);
+const isLoggedIn = (req, res, next) => {
+  if (req.user) {
+    next();
+  } else {
+    res.sendStatus(401);
+  }
+};
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use(logger('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+// auth google
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.clientIDGoogle,
+      clientSecret: process.env.clientSecretGoogle,
+      callbackURL: process.env.callbackURLGoogle,
+      passReqToCallback: true,
+    },
+    (request, accessToken, refreshToken, profile, done) => {
+      winston.info(accessToken);
+      winston.info('profile');
+      winston.info(profile);
+      return done(null, profile);
+    },
+  ),
+);
+passport.serializeUser((user, done) => {
+  done(null, user);
+});
+passport.deserializeUser((user, done) => {
+  done(null, user);
+});
+
+// app.use('/', indexRouter);
 app.use('/running', runningRouter);
 app.use('/crawl', crawlRouter);
+
+app.get('/', (req, res) => {
+  res.send('<a href="/auth/google">Login Google</a>');
+});
+
+app.get(
+  '/auth/google',
+  passport.authenticate('google', {
+    scope: ['email', 'profile'],
+  }),
+);
+
+app.get(
+  '/auth/google/callback',
+  passport.authenticate('google', {
+    successRedirect: '/success',
+    failureRedirect: '/failed',
+  }),
+);
+
+app.get('/failed', (req, res) => {
+  res.json({
+    status: 404,
+    result: 'Failed',
+  });
+});
+app.get('/success', isLoggedIn, (req, res) => {
+  res.json({
+    status: 200,
+    result: 'Welcome',
+  });
+});
+
+app.get('/logout', (req, res) => {
+  req.session = null;
+  req.logout();
+  res.redirect('/');
+});
 
 // // catch 404 and forward to error handler
 app.use((req, res, next) => {
