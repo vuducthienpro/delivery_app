bbd3f0ca9b77130647d00cb8f8a4fce5381b1b07
thienvd-2559 <vu.duc.thien@sun-asterisk.com>
2021-10-08 09:38:54 +0700

    update error table OrderService.ts

--- src/services/OrderService.ts
+++ src/services/OrderService.ts
@@ -4,21 +4,21 @@ import { FilterQuery, DocumentDefinition, UpdateQuery, QueryOptions } from 'mong
 export class OrderService {
   public static getAllOrder = () => {
     return Order.find();
-  }
+  };
 
   public static getOrder = (query: FilterQuery<OrderDocument>) => {
     return Order.findById(query);
-  }
+  };
 
   public static createOrder = async (input: DocumentDefinition<OrderDocument>) => {
     return Order.create(input);
-  }
+  };
 
   public static findAndUpdateOrder = (query: FilterQuery<OrderDocument>, update: UpdateQuery<OrderDocument>, options: QueryOptions) => {
     return Order.findOneAndUpdate(query, update, options);
-  }
+  };
 
   public static deleteOrder = (query: FilterQuery<OrderDocument>) => {
     return Order.deleteOne(query);
-  }
+  };
 }
