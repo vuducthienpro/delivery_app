2a496fd799410976c9c4764949847183fbe73e56
chungpv <chungpv@saisystem.vn>
2021-10-19 01:00:53 +0700

    update image, add history order

--- src/routes/order.ts
+++ src/routes/order.ts
@@ -14,6 +14,7 @@ import authUser from '../middleware/authUser';
 const router = Router();
 
 router.get('/', authAdminAndUser, OrderController.getAllOrder);
+router.get('/history', authAdminAndUser, OrderController.historyOrder);
 router.get('/:id', getOrderByIdSchema, validateRequestSchema, authAdminAndUser, OrderController.getOrderById);
 router.post('/', insertOrderSchema, validateRequestSchema, authUser, OrderController.createOrder);
 router.put('/:id', updateOrderSchema, validateRequestSchema, authAdminAndUser, OrderController.updateOrder);
