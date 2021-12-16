32463d77189ab9c8cddf05eff9a325d2fc24548e
phongit <phongit1995@gmail.com>
2021-12-16 21:32:55 +0700

    add estimate

--- src/models/Product.ts
+++ src/models/Product.ts
@@ -6,7 +6,7 @@ export interface ProductDocument extends mongoose.Document {
   order: number;
   name: string;
   image: string;
-  url:string ;
+  url: string;
   price: number;
   total: number;
   status: string;
@@ -17,7 +17,7 @@ export interface ProductDocument extends mongoose.Document {
   fixedWeight: number;
   confirmed: boolean;
   customerNote: string;
-  customerName:string ;
+  customerName: string;
   staffNote: string;
   orderDate: Date;
   orderNo: string;
@@ -39,6 +39,7 @@ const ProductSchema = new mongoose.Schema(
     totalPrice: { type: Number, required: false },
     status: { type: String, default: EProductStatus.REGISTER_ORDER }, // 0: Out of stock - 1: Stocking
     quantity: { type: Number, default: 0, required: false },
+    estimateArriveFromEcSite: { type: String },
     shipCompany: { type: String },
     description: { type: String },
     estimatedWeight: { type: Number },
@@ -47,7 +48,7 @@ const ProductSchema = new mongoose.Schema(
     fixedWeight: Number,
     confirmed: { type: Boolean, default: false },
     customerNote: { type: String },
-    customerName:{type:String},
+    customerName: { type: String },
     staffNote: String,
     pack: { type: Schema.Types.ObjectId, ref: 'Pack' },
     orderDate: { type: Date, default: Date.now },
