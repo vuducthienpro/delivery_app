f12805ad19591e31b888f099d7b6f1604ee777aa
phongit <phongit1995@gmail.com>
2021-12-20 12:12:46 +0700

    fix cors

--- src/index.ts
+++ src/index.ts
@@ -38,17 +38,14 @@ const app = express();
 // parse various different custom JSON types as JSON
 app.use(bodyParser.json());
 // app.use(cors());
-// app.use(
-//   cors({
-//     origin: true,
-//     credentials: true,
-//   }),
-// );
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+app.use(cors({
+  origin: '*'
+}));
+// app.use(function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   next();
+// });
 app.use(cookieParser());
 
 // upload image
